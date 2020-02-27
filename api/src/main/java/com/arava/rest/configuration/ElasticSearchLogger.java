f58876d14671ffffc8fbaaa031ff02d4d81b66e4
DOGGA Nidhal <dogga.nidhal@gmail.com>
2020-02-27 13:03:26 +0100

    Refactor spring boot configuration Log http requests to elastic search index

---
+++ api/src/main/java/com/arava/rest/configuration/ElasticSearchLogger.java
@@ -0,0 +1,96 @@
+package com.arava.rest.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Created by Nidhal Dogga
+ * Date : 27/02/2020 09:09
+ * All rights reserved.
+ */
+
+@Slf4j
+@ConditionalOnProperty(value = "arava.log-to-elasticsearch", havingValue = "true")
+@Component
+public class ElasticSearchLogger extends OncePerRequestFilter implements ActionListener<IndexResponse> {
+
+  @Autowired
+  private RestHighLevelClient esClient;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    Instant begin = Instant.now();
+    filterChain.doFilter(request, response);
+    Instant end = Instant.now();
+    logRequest(request, response, begin, end);
+
+  }
+
+  @SneakyThrows
+  private void logRequest(HttpServletRequest request, HttpServletResponse response, Instant begin, Instant end) {
+    IndexRequest indexRequest = new IndexRequest("com.arava.web");
+    indexRequest.id(UUID.randomUUID().toString());
+    Map<String, Object> requestMap = new HashMap<String, Object>() {{
+      put("URL", request.getRequestURL());
+      put("Method", request.getMethod());
+      put("Timestamp", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+      put("Headers", Collections.list(request.getHeaderNames()).stream()
+              .reduce(new HashMap<String, String>(),
+                      (headersMap, headerKey) -> new HashMap<String, String>() {{
+                        put(headerKey, request.getHeader(headerKey));
+                      }},
+                      (previous, current) -> new HashMap<String, String>() {{
+                        putAll(current);
+                        putAll(previous);
+                      }})
+      );
+      put("Status", response.getStatus());
+      put("Elapsed", String.format("%d ms", Duration.between(begin, end).toMillis()));
+      if (request.getHeader("User-Agent") != null) {
+        put("User-Agent", request.getHeader("User-Agent"));
+      }
+    }};
+    String jsonString = new ObjectMapper().writeValueAsString(requestMap);
+    indexRequest.source(jsonString, XContentType.JSON);
+    esClient.indexAsync(indexRequest, RequestOptions.DEFAULT, this);
+  }
+
+  @Override
+  public void onResponse(IndexResponse indexResponse) {
+    log.info("Request with id {} was logged. \n Content : {}",
+            indexResponse.getId(),
+            indexResponse.getIndex());
+  }
+
+  @Override
+  public void onFailure(Exception e) {
+    log.error("Failed to log request to elastic search. \n {}", e.getLocalizedMessage());
+  }
+
+}
--- core/src/main/java/com/arava/configuration/CoreConfiguration.java
+++ core/src/main/java/com/arava/configuration/CoreConfiguration.java
@@ -1,6 +1,18 @@
 package com.arava.configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /**
  * Created by Nidhal Dogga
@@ -9,9 +21,37 @@ import org.springframework.context.annotation.Configuration;
  */
 
 
+@Slf4j
 @Configuration
 public class CoreConfiguration {
 
+  @Value("${elasticsearch.host}")
+  public String esHost;
 
+  @Value("${elasticsearch.port}")
+  public Integer esPort;
+
+  @Value("${elasticsearch.protocol}")
+  public String esProtocol;
+
+  @Value("${elasticsearch.username}")
+  public String esUsername;
+
+  @Value("${elasticsearch.password}")
+  public String esPassword;
+
+  @Bean
+  @Primary
+  public RestHighLevelClient elasticSearchClient() {
+    CredentialsProvider esCredentialsProvider = new BasicCredentialsProvider();
+    esCredentialsProvider.setCredentials(AuthScope.ANY,
+            new UsernamePasswordCredentials(esUsername, esPassword));
+    RestClientBuilder builder = RestClient
+            .builder(new HttpHost(esHost, esPort, esProtocol))
+            .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
+                    .setDefaultCredentialsProvider(esCredentialsProvider)
+            );
+    return new RestHighLevelClient(builder);
+  }
 
 }
