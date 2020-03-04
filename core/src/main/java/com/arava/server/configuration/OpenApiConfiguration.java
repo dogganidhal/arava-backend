0acd81656679cfb0e539140121883fe73abe4bd0
DOGGA Nidhal <dogga.nidhal@gmail.com>
2020-03-04 09:26:29 +0100

    Fix OpenApi swagger server url generation

--- core/src/main/java/com/arava/server/configuration/OpenApiConfiguration.java
+++ core/src/main/java/com/arava/server/configuration/OpenApiConfiguration.java
@@ -3,6 +3,8 @@ package com.arava.server.configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,9 +21,15 @@ public class OpenApiConfiguration {
 
   public static final String securitySchemeName = "JWT Token";
 
+  @Value("${arava.server.url:http://localhost:${server.port:8080}}")
+  private String serverUrl;
+
   @Bean
   public OpenAPI openApi() {
     return new OpenAPI()
+            .addServersItem(new Server()
+                    .url(serverUrl)
+            )
             .components(new Components()
                     .addSecuritySchemes(securitySchemeName, new SecurityScheme()
                             .type(SecurityScheme.Type.HTTP)
